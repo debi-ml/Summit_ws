1dd96b8d3ed92aa9e2b601c862aa3027083b39a5
matej <matej.loris16@gmail.com>
2023-05-03 22:58:18 +0200

    ignore build

--- build/summit_navigation/CMakeFiles/summit_navigation_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/summit_navigation_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/summit_navigation_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/summit_navigation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/matej/Desktop/summit_xl_ws/src/summit_navigation
-# Build directory: /home/matej/Desktop/summit_xl_ws/build/summit_navigation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
