672931de4a85c9c44594ff848390cdaaffdd15a5
matej <matej.loris16@gmail.com>
2023-05-03 13:02:15 +0200

    commit

--- install/setup.ps1
+++ install/setup.ps1
@@ -21,6 +21,10 @@ function _colcon_prefix_chain_powershell_source_script {
   }
 }
 
+# source chained prefixes
+_colcon_prefix_chain_powershell_source_script "/opt/ros/foxy\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/matej/IsaacSim-ros_workspaces/foxy_ws/install\local_setup.ps1"
+
 # source this prefix
 $env:COLCON_CURRENT_PREFIX=(Split-Path $PSCommandPath -Parent)
 _colcon_prefix_chain_powershell_source_script "$env:COLCON_CURRENT_PREFIX\local_setup.ps1"
